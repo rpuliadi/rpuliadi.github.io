61f1724e66b16e2d7b4d729a3b4a8f478af0fbe3
Ramya P <rpuliadi@gmail.com>
2024-09-24 02:00:40 +0000

    updated contents

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,23 +8,22 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Ramya </span>
+            from <span className="purple"> USA.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am Data Engineer focused on building data platforms.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I am passionate about finding ways to incorporate ML and AI in Data Engineering.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Crocheting
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Cooking
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -34,7 +33,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Ramya</footer>
         </blockquote>
       </Card.Body>
     </Card>
